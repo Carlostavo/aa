6f718a32d125e57283f520654c11d2e7561d0594
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-05 17:31:21 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,10 @@
 import '../styles/globals.css'
 import Nav from '../components/Nav'
 import Head from 'next/head'
-import { EditProvider } from '../contexts/EditContext'
 
 export default function App({ Component, pageProps }) {
   return (
-    <EditProvider>
+    <>
       <Head>
         <title>Gestión de Residuos Sólidos</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.3/css/all.min.css" />
@@ -16,6 +15,6 @@ export default function App({ Component, pageProps }) {
           <Component {...pageProps} />
         </main>
       </div>
-    </EditProvider>
+    </>
   )
 }
