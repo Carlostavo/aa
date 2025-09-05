f0f900b3ba8817e0a362c4db6f0baf18d00cda79
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-05 16:48:51 -0500

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,16 +3,19 @@ import Login from './Login'
 
 export default function Nav(){
   return (
-    <nav className="flex justify-between items-center">
-      <div className="flex gap-6 font-semibold">
-        <Link href="/">Inicio</Link>
-        <Link href="/indicadores">Indicadores</Link>
-        <Link href="/metas">Metas</Link>
-        <Link href="/avances">Avances</Link>
-        <Link href="/reportes">Reportes</Link>
-        <Link href="/formularios">Formularios</Link>
+    <nav className="topbar">
+      <div className="brand">
+        <i className="fa-solid fa-recycle fa-xl"></i> Gestión RS
       </div>
-      <Login />
+      <div className="nav">
+        <Link href="/" className="nav-link active" data-page="home">Inicio</Link>
+        <Link href="/indicadores" className="nav-link" data-page="indicadores">Indicadores</Link>
+        <Link href="/metas" className="nav-link disabled" data-page="metas">Metas</Link>
+        <Link href="/avances" className="nav-link disabled" data-page="avances">Avances</Link>
+        <Link href="/reportes" className="nav-link disabled" data-page="reportes">Reportes</Link>
+        <Link href="/formularios" className="nav-link" data-page="formularios">Formularios</Link>
+        <Login />
+      </div>
     </nav>
   )
 }
