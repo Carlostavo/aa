aca79738717019559edbe1d9ffc30a9cf19e7ca6
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-05 16:52:21 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,20 @@
 import '../styles/globals.css'
 import Nav from '../components/Nav'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Nav />
-      <main className="flex-1 p-6 max-w-6xl mx-auto w-full">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <>
+      <Head>
+        <title>Gestión de Residuos Sólidos</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.3/css/all.min.css" />
+      </Head>
+      <div className="min-h-screen flex flex-col">
+        <Nav />
+        <main className="flex-1 p-6 max-w-6xl mx-auto w-full">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </>
   )
 }
