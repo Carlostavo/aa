4d0c8db5a003ee7ba54c73fc3f2609cc19c33e57
Carlostavo <46359518+Carlostavo@users.noreply.github.com>
2025-09-05 20:12:43 -0500

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,22 @@
-import '../styles/globals.css'
-import Nav from '../components/Nav'
-import Head from 'next/head'
+import '../styles/globals.css';
+import Nav from '../components/Nav';
+import { EditorProvider } from '../contexts/EditorContext';
+import { useRouter } from 'next/router';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const pageSlug = router.pathname === '/' ? 'inicio' : router.pathname.slice(1);
+
   return (
-    <>
-      <Head>
-        <title>Gestión de Residuos Sólidos</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.3/css/all.min.css" />
-      </Head>
-      <div className="min-h-screen flex flex-col">
+    <EditorProvider pageSlug={pageSlug}>
+      <div className="app">
         <Nav />
         <main className="flex-1 p-6 max-w-6xl mx-auto w-full">
           <Component {...pageProps} />
         </main>
       </div>
-    </>
-  )
+    </EditorProvider>
+  );
 }
+
+export default MyApp;
